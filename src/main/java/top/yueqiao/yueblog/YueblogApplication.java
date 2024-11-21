91cfa93dab90e4fcd6299c17a8070b7e669b677a
yueqiao <yueqiaozgb@163.com>
2024-11-22 00:35:22 +0800

    1.创建博客项目

---
+++ src/main/java/top/yueqiao/yueblog/YueblogApplication.java
@@ -0,0 +1,17 @@
+package top.yueqiao.yueblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yueqiao
+ * @date 2024/11/22
+ */
+@SpringBootApplication
+public class YueblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YueblogApplication.class, args);
+	}
+
+}

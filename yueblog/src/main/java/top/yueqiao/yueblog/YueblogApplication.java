b90f84e02a9fe6fa059d65c21834ea3294c9151a
yueqiao <yueqiaozgb@163.com>
2024-12-15 23:24:07 +0800

    create yueblog project

---
+++ yueblog/src/main/java/top/yueqiao/yueblog/YueblogApplication.java
@@ -0,0 +1,13 @@
+package top.yueqiao.yueblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YueblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YueblogApplication.class, args);
+    }
+
+}
---
+++ yueblog/src/test/java/top/yueqiao/yueblog/YueblogApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yueqiao.yueblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YueblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
